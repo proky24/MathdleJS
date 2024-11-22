2a9d67fdde45a5e75814dc1deba3499536e74046
Tomáš Prokeš <prokest17@gmail.com>
2024-11-22 22:58:36 +0100

    fully function!!
    without placement

--- index.js
+++ index.js
@@ -18,9 +18,13 @@ const characters = [
 
 let guesses = [];
 let rightOrWrong = [];
+let secondRigthOrWrong = [];
+let thirdRightOrWrong = [];
+let fourthRightOrWrong = [];
 let array = [];
 let tries = 1;
 let indexOfGuesses = 0;
+let guessIndex = 9;
 
 const problem = problems[Math.floor(Math.random() * problems.length)];
 console.log(problem);
@@ -36,7 +40,7 @@ for(let i = 0; i < characters.length; i++) {
 
 pushChracter = (character) => {
     guesses[indexOfGuesses] = character;
-    document.querySelectorAll('.guess')[indexOfGuesses].innerText = guesses[indexOfGuesses];
+    document.querySelectorAll('.guess')[indexOfGuesses].innerHTML = character;
     if(indexOfGuesses === 8 || indexOfGuesses === 17 || indexOfGuesses === 26 || indexOfGuesses === 35 || indexOfGuesses === 44) {
         console.log('compare useffect indexofguesses === 9 //////////');
         compare(tries);  
@@ -59,7 +63,12 @@ const compare = (tries) => {
                   rightOrWrong[i] = 'wrong';
                   document.querySelectorAll('.guess')[i].style.backgroundColor = 'red';
                   console.log(rightOrWrong);
-                } else {
+                } /*    else if(problem[i].includes == guesses[i]) {
+                    console.log('placement');
+                    rightOrWrong[i] = 'placement';
+                    document.querySelectorAll('.guess')[i].style.backgroundColor = 'yellow';
+                    console.log(rightOrWrong);
+                } */else {
                   console.log('correct');
                   rightOrWrong[i] = 'correct';
                   document.querySelectorAll('.guess')[i].style.backgroundColor = 'green';
@@ -71,18 +80,24 @@ const compare = (tries) => {
         break;
 
         case 2:
-            for(let i = problem.length; i < problem.length + problem.length; i++){
-                if(problem[i] !== guesses[i]){
+            for(let i = 0; i < problem.length; i++){
+                if(problem[i] !== guesses[guessIndex]){
                   console.log('wrong');
-                  rightOrWrong[i] = 'wrong';
-                  document.querySelectorAll('.guess')[i].style.backgroundColor = 'red';
-                  console.log(rightOrWrong);
-                } else {
+                  secondRigthOrWrong[i] = 'wrong';
+                  document.querySelectorAll('.secondGuess')[i].style.backgroundColor = 'red';
+                  console.log(secondRigthOrWrong);
+                }/* else if(problem[i].includes == guesses[guessIndex]) {
+                    console.log('placement');
+                    rightOrWrong[i] = 'placement';
+                    document.querySelectorAll('.secondGuess')[i].style.backgroundColor = 'yellow';
+                    console.log(rightOrWrong);
+                } */ else {
                   console.log('correct');
-                  rightOrWrong[i] = 'correct';
-                  document.querySelectorAll('.guess')[i].style.backgroundColor = 'green';
-                  console.log(rightOrWrong);
+                  secondRigthOrWrong[i] = 'correct';
+                  document.querySelectorAll('.secondGuess')[i].style.backgroundColor = 'green';
+                  console.log(secondRigthOrWrong);
                 }
+                guessIndex++;
             }
 
             checkStatus();
@@ -90,107 +105,106 @@ const compare = (tries) => {
         break;
 
         case 3:
-            for(let i = problem.length + problem.length; i < problem.length + problem.length + problem.length; i++){
-                if(problem[i] !== guesses[i]){
+            for(let i = 0; i < problem.length ; i++){
+                if(problem[i] !== guesses[guessIndex]){
                   console.log('wrong');
-                  rightOrWrong[i] = 'wrong';
-                  document.querySelectorAll('.guess')[i].style.backgroundColor = 'red';
-                  console.log(rightOrWrong);
+                  thirdRightOrWrong[i] = 'wrong';
+                  document.querySelectorAll('.thirdGuess')[i].style.backgroundColor = 'red';
+                  console.log(thirdRightOrWrong);
                 } else {
                   console.log('correct');
-                  rightOrWrong[i] = 'correct';
-                  document.querySelectorAll('.guess')[i].style.backgroundColor = 'green';
-                  console.log(rightOrWrong);
+                  thirdRightOrWrong[i] = 'correct';
+                  document.querySelectorAll('.thirdGuess')[i].style.backgroundColor = 'green';
+                  console.log(thirdRightOrWrong);
                 }
+                guessIndex++;
             }
 
             checkStatus();
-
+            
         break;
 
         case 4:
-            for(let i = problem.length + problem.length + problem.length; i < problem.length + problem.length + problem.length + problem.length; i++){
-                if(problem[i] !== guesses[i]){
+            for(let i = 0; i < problem.length; i++){
+                if(problem[i] !== guesses[guessIndex]){
                   console.log('wrong');
-                  rightOrWrong[i] = 'wrong';
-                  document.querySelectorAll('.guess')[i].style.backgroundColor = 'red';
-                  console.log(rightOrWrong);
+                  fourthRightOrWrong[i] = 'wrong';
+                  document.querySelectorAll('.fourthGuess')[i].style.backgroundColor = 'red';
+                  console.log(fourthRightOrWrong);
                 } else {
                   console.log('correct');
-                  rightOrWrong[i] = 'correct';
-                  document.querySelectorAll('.guess')[i].style.backgroundColor = 'green';
-                  console.log(rightOrWrong);
+                  fourthRightOrWrong[i] = 'correct';
+                  document.querySelectorAll('.fourthGuess')[i].style.backgroundColor = 'green';
+                  console.log(fourthRightOrWrong);
                 }
-            }
-
-            checkStatus();
-
-        break;
-
-        case 5: 
-            for(let i = problem.length + problem.length + problem.length + problem.length; i < problem.length + problem.length + problem.length + problem.length + problem.length; i++){
-                if(problem[i] !== guesses[i]){
-                    console.log('wrong');
-                    rightOrWrong[i] = 'wrong';
-                    document.querySelectorAll('.guess')[i].style.backgroundColor = 'red';
-                    console.log(rightOrWrong);
-                } else {
-                    console.log('correct');
-                    rightOrWrong[i] = 'correct';
-                    document.querySelectorAll('.guess')[i].style.backgroundColor = 'green';
-                    console.log(rightOrWrong);
-                }
+                guessIndex++;
             }
 
             setTimeout(() => {
                 checkStatus();  
-            }, 2000);
+            }, 1500);
 
         break;
-
-
     }
-
-
-    /*for(let i = 0; i < problem.length; i++){
-      if(problem[i] !== guesses[i]){
-        console.log('wrong');
-        rightOrWrong[i] = 'wrong';
-        document.querySelectorAll('.guess')[i].style.backgroundColor = 'red';
-        console.log(rightOrWrong);
-      } else {
-        console.log('correct');
-        rightOrWrong[i] = 'correct';
-        document.querySelectorAll('.guess')[i].style.backgroundColor = 'green';
-        console.log(rightOrWrong);
-      }
-    }*/
 }
 
 const checkStatus = () => {
-    for(let i = 0; i < 9; i++) {
-        const div = document.querySelectorAll('.guess')[i];
-        array.push(div);
+    switch (tries) {
+        case 1:
+            for(let i = 0; i < 9; i++) {
+                const div = document.querySelectorAll('.guess')[i];
+                array.push(div);
+            }
+            finalFunction();
+            array = [];
+        break;
+
+        case 2:
+            for(let i = 0; i < 9; i++) {
+                const div = document.querySelectorAll('.secondGuess')[i];
+                array.push(div);
+            }
+            finalFunction();
+            array = [];
+        break;
+
+        case 3: 
+            for(let i = 0; i < 9; i++) {
+                const div = document.querySelectorAll('.thirdGuess')[i];
+                array.push(div);
+            }
+            finalFunction();
+            array = [];
+        break;
+
+        case 4:
+            for(let i = 0; i < 9; i++) {
+                const div = document.querySelectorAll('.fourthGuess')[i];
+                array.push(div);
+            }
+            finalFunction();
+            array = [];
+        break;
     }
+}
+
+const finalFunction = () => {
+
     if(array.some((element) => element.style.backgroundColor === 'red')) {
         console.log('wrong');
+        indexOfGuesses++;
         tries++;
         if(tries === 5) {
             alert('game over');
             window.location.reload();
         }
         console.log(tries);
-        /*if(tries === 3) {
-            console.log('game over');
-            document.querySelector('.game-over').style.display = 'block';
-        } else {
-            console.log('try again');
-            document.querySelector('.try-again').style.display = 'block';
-        }*/
+        array = [];
     }  else {
         setTimeout(() => {
             window.location.reload();
             alert('you won');
         }, 1500);
     }
+
 }
